c1a40de3232f9211cd3ad0c223330433a3f3e36e
Subhan Masimov <subhanmasimov02@gmail.com>
2021-05-28 11:10:54 +0400

    camp 8th day

---
+++ src/main/java/kodlamaio/hrms/entities/concretes/Candidate.java
@@ -0,0 +1,39 @@
+package kodlamaio.hrms.entities.concretes;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@EqualsAndHashCode(callSuper = false)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "candidates")
+public class Candidate extends User{
+
+	@Id
+	@Column(name = "user_id")
+	private int userId;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "nationality_id")
+	private String nationalityId;
+
+	@Column(name = "birth_date")
+	private LocalDate birthDate;
+
+}
---
+++ src/main/java/kodlamaio/hrms/entities/concretes/Company.java
@@ -0,0 +1,33 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@EqualsAndHashCode(callSuper = false)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "companies")
+public class Company extends User{
+
+	@Id
+	@Column(name = "user_id")
+	private int userId;
+
+	@Column(name = "company_name")
+	private String companyName;
+
+	@Column(name = "website")
+	private String website;
+
+	@Column(name = "phone_number")
+	private String phoneNumber;
+}
